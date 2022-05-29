a2aff459dd863c7bfb8a3a2927b8f1022701081f
Luca2713 <lucaauet@gmail.com>
2022-05-29 18:28:37 -0300

    feat: GUI Ready

--- Api/src/funciones.c
+++ Api/src/funciones.c
@@ -2,101 +2,95 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <string.h>
-
 #include <ulfius.h>
 #include <jansson.h>
 #include <unistd.h>
-
 
 /**
  * Callback function for the web application on /helloworld url call
  */
 
-static int gpio(const struct _u_request * request,
-    struct _u_response * response, void * user_data) 
+static int gpio(const struct _u_request *request,
+                struct _u_response *response, void *user_data)
 {
-  (void) request;
-  (void) user_data;
+    (void)request;
+    (void)user_data;
 
-  int fd;
-  char path[1024];
-  char readBuf[2];
+    int fd;
+    char path[1024];
+    char readBuf[2];
 
+    json_t *json_request = ulfius_get_json_body_request(request, NULL);
 
-  json_t * json_request = ulfius_get_json_body_request(request, NULL);
+    if (json_request == NULL)
+    {
+        long long status = 500;
+        json_int_t *j_status = &status;
 
-  if (json_request == NULL) 
-  {
-    long long status = 500;
+        json_t *json_body = json_object();
+        json_object_set_new(json_body, "code", json_integer(*j_status));
+        json_object_set_new(json_body, "description",
+                            json_string("No se ingreso datos"));
+
+        ulfius_set_json_body_response(response, (uint)status, json_body);
+        json_decref(json_body);
+        json_decref(json_request);
+
+        return 1;
+    }
+
+    int gpio = (int)json_integer_value(json_object_get(json_request, "in"));
+
+    snprintf(path, sizeof(path), "/dev/raspiGpio%d", gpio);
+    fd = open(path, O_RDWR);
+
+    if (read(fd, readBuf, 1) < 1)
+    {
+        perror("write, set pin input");
+        return U_CALLBACK_ERROR;
+    }
+
+    readBuf[1] = '\0';
+
+    long long valor = atoll(readBuf);
+
+    json_t *json_body = NULL;
+
+    json_int_t *out = &valor;
+
+    long long status = 200;
     json_int_t *j_status = &status;
 
-    json_t *json_body = json_object();
+    json_body = json_object();
     json_object_set_new(json_body, "code", json_integer(*j_status));
-    json_object_set_new(json_body, "description",
-        json_string("No se ingreso datos"));
+    json_object_set_new(json_body, "out", json_integer(*out));
+
+    ulfius_add_header_to_response(response, ".json", "application/json");
+    ulfius_set_json_body_response(response, (uint)status, json_body);
+    json_decref(json_body);
+
+    close(fd);
+
+    return U_CALLBACK_CONTINUE;
+}
+
+int no_encontrado(const struct _u_request *request,
+                  struct _u_response *response, void *user_data)
+{
+    (void)request;
+    (void)user_data;
+
+    json_t *json_body = NULL;
+
+    long long status = 404;
+    json_int_t *j_status = &status;
+
+    json_body = json_object();
+    json_object_set_new(json_body, "code", json_integer(*j_status));
+    json_object_set_new(json_body, "description", json_string("NOT FOUND"));
 
     ulfius_set_json_body_response(response, (uint)status, json_body);
     json_decref(json_body);
-    json_decref(json_request);
 
-    return 1;
-  }
-
-  int gpio = (int)json_integer_value(json_object_get(json_request,"in"));
-
-  snprintf(path, sizeof(path), "/dev/raspiGpio%d", gpio);
-  fd = open(path, O_RDWR);
-
-  if (read(fd, readBuf, 1) < 1)
-  {
-    perror("write, set pin input");
-    exit(EXIT_FAILURE);
-  }
-
-  readBuf[1] = '\0';
-
-  long long valor = atoll(readBuf);
-
-  json_t * json_body = NULL;
-
-  json_int_t * out = &valor;
-
-  long long status = 200;
-  json_int_t *j_status = &status;
-
-  json_body = json_object();
-  json_object_set_new(json_body, "code", json_integer(*j_status));
-  json_object_set_new(json_body, "out", json_integer(*out));
-
-  ulfius_add_header_to_response(response, ".json", "application/json");
-  ulfius_set_json_body_response(response, (uint)status, json_body);
-  json_decref(json_body);
-
-  close(fd);
-
-  return U_CALLBACK_CONTINUE;
+    return U_CALLBACK_CONTINUE;
 }
-
-
-int no_encontrado(const struct _u_request * request,
-    struct _u_response * response, void * user_data) 
-{
-  (void) request;
-  (void) user_data;
-
-  json_t * json_body = NULL;
-
-  long long status = 404;
-  json_int_t *j_status = &status;
-
-  json_body = json_object();
-  json_object_set_new(json_body, "code", json_integer(*j_status));
-  json_object_set_new(json_body, "description", json_string("NOT FOUND"));
-
-  ulfius_set_json_body_response(response, (uint)status, json_body);
-  json_decref(json_body);
-
-  return U_CALLBACK_CONTINUE;
-}
-
-
---
+++ GUI/finalGui.py
@@ -0,0 +1,95 @@
+import PySimpleGUI as sg
+import numpy as np
+import requests
+import json
+from random import randint
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, FigureCanvasAgg
+from matplotlib.figure import Figure
+import time
+
+url = 'http://192.168.100.232:6666/puerto'
+port = 20
+time_s = 0
+x_vals=np.zeros(10)
+y_vals=np.zeros(10)
+
+def get_sensor_data():
+    global time_s
+    global x_vals
+    global y_vals
+    global port
+    
+    x_vals = np.roll(x_vals,-1)
+    x_vals[9]=time_s
+    time_s+=1
+
+    y_vals = np.roll(y_vals,-1)
+    
+    req = {'in':port}
+    x = requests.get(url, json = req)
+    res = json.loads(x.text)
+
+    if(res["code"]==200):
+        print("ok")
+        print(y_vals)
+        y_vals[9] = res["out"]
+    
+    else:
+        print("Error")
+
+def draw_figure(canvas, figure, loc=(0, 0)):
+    figure_canvas_agg = FigureCanvasTkAgg(figure, canvas)
+    figure_canvas_agg.draw()
+    figure_canvas_agg.get_tk_widget().pack(side='top', fill='both', expand=1)
+    return figure_canvas_agg
+
+def main():
+    global port
+
+    # define the form layout
+    layout = [[sg.Text('Animated Matplotlib', size=(40, 1),
+                justification='center', font='Helvetica 20')],
+              [sg.Canvas(size=(640, 480), key='-CANVAS-')],
+              [sg.Button('Toggle', size=(10, 1), pad=((280, 0), 3), font='Helvetica 14')]]
+
+    # create the form and show it without the plot
+    window = sg.Window('Demo Application - Embedding Matplotlib In PySimpleGUI',
+                layout, finalize=True)
+
+    canvas_elem = window['-CANVAS-']
+    canvas = canvas_elem.TKCanvas
+
+    # draw the initial plot in the window
+    fig = Figure()
+    ax = fig.add_subplot(111)
+    ax.set_xlabel("X axis")
+    ax.set_ylabel("Y axis")
+   
+    ax.set_xlim(time_s - 10,time_s)
+    ax.grid()
+
+    fig_agg = draw_figure(canvas, fig)
+
+    while(True):
+
+        event, values = window.read(timeout=10)
+        
+        if(event == "Toggle"):
+            if(port==21):
+                port=20
+            else:
+                port=21
+    
+        if(event == sg.WIN_CLOSED):
+            exit()
+
+        get_sensor_data()
+
+        ax.cla()                    # clear the subplot
+        ax.grid()                   # draw the grid
+        ax.plot(x_vals, y_vals,  color='purple')
+        fig_agg.draw()
+        time.sleep(1)
+
+if __name__ == '__main__':
+    main()

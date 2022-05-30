6c116b10d889c39599ca0f15629627a294354776
Juan Manuel Gil <juanmanuelgil99@gmail.com>
2022-05-29 21:48:24 -0300

    Improvements: más logs para el run script

--- run.sh
+++ run.sh
@@ -3,22 +3,24 @@
 #archivos relacionados al kernel de linux, y los correspondientes módulos de python.
 
 #Cargo variables de entorno
+printf "LOADING env vars...\n"
 export IP_ADD=192.168.46.1
 export PORT=6666
 export BASE_URL=http://$IP_ADD:$PORT/
 export ENDPOINT=puerto 
 export RASPI_P1=20
 export RASPI_P2=21
+printf "Env vars LOADED\n"
 
 #Borro los archivos
 
-printf "Cleaning old files...\n"
+printf "CLEANING old files...\n"
 cd char_device_driver
 make clean
 cd ../api
 make clean
 cd ..
-printf "Files REMOVED"
+printf "Old files REMOVED"
 
 #Compilo el código en c
 printf "c code COMPILING...\n"
